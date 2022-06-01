6f260abea9fe03e0e8c79cd39e3c2dbba203bd7b
praneethtkonda <praneethtkonda@gmail.com>
2022-06-01 09:25:04 +0530

    Enjoy maadi

--- server/helper/otp.py
+++ server/helper/otp.py
@@ -1,7 +1,33 @@
 from random import randint
 
+import os
+import requests
+import json
+
+SMS_UPSTREAM_URL = "https://www.fast2sms.com/dev/bulkV2"
+
 def generate_otp(num_digits):
-    otp = ''
+    otp = ""
     for _ in range(num_digits):
-        otp += str(randint(0,9))
+        otp += str(randint(0, 9))
     return otp
+
+def send_otp_sms(otp, mobilenumber):
+    api_key = os.environ.get("OTP_KEY", "")
+    if api_key == "":
+        return False
+    payload = json.dumps(
+        {
+            "route": "otp",
+            "variables_values": otp,
+            "language": "english",
+            "numbers": mobilenumber,
+        }
+    )
+    headers = {
+        "Authorization": api_key,
+        "Content-Type": "application/json",
+    }
+
+    response = requests.request("POST", SMS_UPSTREAM_URL, headers=headers, data=payload)
+    return response.text
